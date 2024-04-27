ba04a1621f4f1b1bb9d1257deaa3c7c924380e5a
roostiq <roostiq@gmail.com>
2024-04-28 01:30:30 +0300

    client init

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { 
+  defineComponent,
+  defineProps,
+} from 'vue';
+import WebSocket from 'ws';
+
+const props = defineProps<{
+  foo: string
+  bar?: number
+}>()
+
+let helloWorld = "Hello World";
+
+interface User {
+  name: string;
+  id: number;
+}
+
+const user: User = {
+  username: "Hayes",
+  id: 'lalala',
+};
+
+</script>
+
